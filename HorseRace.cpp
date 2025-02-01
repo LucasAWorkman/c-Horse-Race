51cdf0b0cfe65a5f8422c9896a220e6d026c87cb
LucasAWorkman <lucas.workman@bsu.edu>
2025-02-01 01:32:00 +0000

    .

--- HorseRace.cpp
+++ HorseRace.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <random>
 using namespace std;
 
 void advance(int hn, int* horses){
-  int coin = rand() % 2;
-  cout<<coin<<endl;
+  if (rand() % 2 == 1) {
+    horses[hn]++;
+    }
 }
 
 void printLine(int hn, int* horses){
-
 }
 
 bool isWinner(int hn, int* horses){
@@ -19,11 +18,14 @@ bool isWinner(int hn, int* horses){
 }
 
 int main(){
-  char track[15];
-  char horses[5];
-  
-  for (int i = 0; i < 5; i++){
-      advance(i, i);
-    }
+  int horses[5] = {0, 0, 0, 0, 0};
+  bool keepgoing = true;
+
+  while (keepgoing) {
+
+    for (int i = 0; i < 5; i++){
+        advance(i, horses);
+      }
+   }
   return 0;
 }
