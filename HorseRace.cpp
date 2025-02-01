6763a94e6f91380e3e59230903b768dd335db2fc
LucasAWorkman <lucas.workman@bsu.edu>
2025-02-01 01:54:59 +0000

    .

--- HorseRace.cpp
+++ HorseRace.cpp
@@ -9,15 +9,31 @@ void advance(int hn, int* horses){
     }
 }
 
-void printLine(int hn, int* horses){
+void printLine(int hn, int* horses) {
+    char track[15]; 
+    for (int i = 0; i < 15; i++) {
+        track[i] = '.';
+    }
+    track[horses[hn]] = '1' + hn;
+
+    for (int i = 0; i < 15; i++) {
+        cout << track[i];
+    }
+    cout << endl; 
 }
 
-bool isWinner(int hn, int* horses){
-
-  return 0;
+bool isWinner(int* horses){
+   for (int i = 0; i < 5; i++) {
+     if (horses[i] == 14) {
+         cout << "Horse " << i + 1 << " wins!" << endl;
+         return true;
+      }
+  }
+    return false;
 }
 
 int main(){
+  srand(time(0));
   int horses[5] = {0, 0, 0, 0, 0};
   bool keepgoing = true;
 
@@ -26,6 +42,15 @@ int main(){
     for (int i = 0; i < 5; i++){
         advance(i, horses);
       }
+      for (int i = 0; i < 5; i++) {
+         printLine(i, horses); 
+       }
+
+    if (isWinner(horses)){
+       keepgoing = false;
+     }
+
+    cin.get();
    }
   return 0;
 }
